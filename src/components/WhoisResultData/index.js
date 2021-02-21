f8fc4caaee8bd30b7af8499417f7844beda494f3
benfiratkaya <benfiratkaya@gmail.com>
2021-02-21 21:57:44 +0300

    Fixed responsive whois result table.

--- src/components/WhoisResultData/index.js
+++ src/components/WhoisResultData/index.js
@@ -12,61 +12,67 @@ const WhoisResultData = ({ result }) => {
             {t("title", { domain: data.domain })}
           </div>
           <div className="card-body">
-            <table className="table">
-              <tbody>
-                <tr>
-                  <th scope="row" className="border-0">
-                    {t("status")}
-                  </th>
-                  <td className="border-0">
-                    <span className="badge badge-danger">
-                      {t("unavailable")}
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("register-date")}</th>
-                  <td>{moment(data.creationDate).format("MM/DD/YYYY")}</td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("update-date")}</th>
-                  <td>{moment(data.updatedDate).format("MM/DD/YYYY")}</td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("expiry-date")}</th>
-                  <td>
-                    {moment(data.registrarRegistrationExpirationDate).format(
-                      "MM/DD/YYYY"
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("remaining-time")}</th>
-                  <td>
-                    {moment(data.registrarRegistrationExpirationDate).diff(
-                      moment(),
-                      "days"
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("registrar")}</th>
-                  <td>
-                    <a href={data.registrarUrl} target="_blank" rel="nofollow">
-                      {data.registrar}
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">{t("nameservers")}</th>
-                  <td>
-                    {data.nameServer.split(" ").map((nameServer) => (
-                      <span className="d-block">{nameServer}</span>
-                    ))}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="table-responsive">
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th scope="row" className="border-0">
+                      {t("status")}
+                    </th>
+                    <td className="border-0">
+                      <span className="badge badge-danger">
+                        {t("unavailable")}
+                      </span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("register-date")}</th>
+                    <td>{moment(data.creationDate).format("MM/DD/YYYY")}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("update-date")}</th>
+                    <td>{moment(data.updatedDate).format("MM/DD/YYYY")}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("expiry-date")}</th>
+                    <td>
+                      {moment(data.registrarRegistrationExpirationDate).format(
+                        "MM/DD/YYYY"
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("remaining-time")}</th>
+                    <td>
+                      {moment(data.registrarRegistrationExpirationDate).diff(
+                        moment(),
+                        "days"
+                      )}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("registrar")}</th>
+                    <td>
+                      <a
+                        href={data.registrarUrl}
+                        target="_blank"
+                        rel="nofollow"
+                      >
+                        {data.registrar}
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">{t("nameservers")}</th>
+                    <td>
+                      {data.nameServer.split(" ").map((nameServer) => (
+                        <span className="d-block">{nameServer}</span>
+                      ))}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
             <pre className="whois-raw-result">{data.rawData}</pre>
           </div>
         </div>
