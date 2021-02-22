d4eea71e78f17e936609ef0918dfc328714a5257
benfiratkaya <benfiratkaya@gmail.com>
2021-02-22 12:48:15 +0300

    Fixed ThemeSwitcher component bug.

--- src/components/ThemeSwitcher/index.js
+++ src/components/ThemeSwitcher/index.js
@@ -1,19 +1,26 @@
-import useTranslation from "next-translate/useTranslation";
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
+import useTranslation from "next-translate/useTranslation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ThemeSwitcher = () => {
   const { t } = useTranslation("common");
+
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-  const themeIcon = theme === "light" ? "moon" : "sun";
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
     <div
       className="d-flex align-items-center"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <div className="icon">
-        <FontAwesomeIcon icon={["fas", themeIcon]} />
+        <FontAwesomeIcon icon={["fas", theme === "light" ? "moon" : "sun"]} />
       </div>
       <span className="d-lg-none ml-2">{t("change-theme")}</span>
     </div>
